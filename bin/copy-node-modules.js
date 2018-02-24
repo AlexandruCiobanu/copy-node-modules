6bb285643458018d627a39e7c10c6beee181d34f
AlexandruCiobanu <AlexandruCiobanu@users.noreply.github.com>
2018-02-24 21:45:44 +0200

    Add include arg

--- bin/copy-node-modules.js
+++ bin/copy-node-modules.js
@@ -5,20 +5,30 @@ var args = process.argv.slice(2);
 
 if (args.length < 2)
 {
-    console.error('Usage: copy-node-module src_dir dest_dir [--dev] [-v|--verbose] ');
+    console.error('Usage: copy-node-module src_dir dest_dir [--dev] [-v|--verbose] [--include <comma_separated_package_list>] ');
     process.exit(1);
 }
 
 var srcDir = args[0].trim(),
     dstDir = args[1].trim(),
     devDeps = false,
-    verbose = false;
+    verbose = false,
+    packageList = [];
 
 // parse input arguments
 if (args.indexOf('--dev') !== -1)
     devDeps = true;
 if (args.indexOf('-v') !== -1 || args.indexOf('--verbose') !== -1)
     verbose = true;
+
+if (args.indexOf("--include") !== -1){
+    var packages = args[args.indexOf("--include") + 1];
+    if(!packages){
+        console.error('No included packages provided');
+        process.exit(1);
+    }
+    packageList = packages.split(",");
+}
 
 if (!path.isAbsolute(srcDir))
     srcDir = path.resolve(process.cwd(), srcDir);
@@ -27,6 +37,9 @@ if (!path.isAbsolute(srcDir))
 
 
 var options = {devDependencies: devDeps};
+if(packageList.length > 0){
+    options.include = packageList;
+}
 copyNodeModules(srcDir, dstDir, options, function(err, packages) {
     if (err)
     {
